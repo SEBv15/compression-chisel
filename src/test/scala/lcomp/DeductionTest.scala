f2f950ac5c8d6ef06f9d2879e9a85034f7c1812b
Sebastian Strempfer <sebastian@strempfer.com>
2021-01-17 02:25:12 -0600

    Comments

--- src/test/scala/lcomp/DeductionTest.scala
+++ src/test/scala/lcomp/DeductionTest.scala
@@ -11,6 +11,7 @@ class DeductionTest extends FlatSpec with ChiselScalatestTester with Matchers {
     it should "test-deduction" in {
         // test case body here
         test(new Deduction(64)) { c =>
+            // Load pre-reduced data
             val reduced = new ListBuffer[Int]()
             val src = Source.fromFile("reduced.txt")
             for (n <- src.getLines) {
@@ -18,15 +19,10 @@ class DeductionTest extends FlatSpec with ChiselScalatestTester with Matchers {
             }
             src.close()
 
-            val solution = new ListBuffer[Int]()
-            val src2 = Source.fromFile("solution.txt")
-            for (n <- src2.getLines) {
-                solution.append(n.toInt)
-            }
-            src2.close()
-
+            // Create a list where the output will be stored (bit oversized, but works)
             val out = ListBuffer.fill((reduced.length/(1024.0/16.0)).ceil.toInt * 64 + 64)(0)
 
+            // Go through the reduced data in aurora-sized blocks and put them in the deductor
             for (i <- 0 until (reduced.length/(1024/16)).ceil.toInt) {
                 val w = ListBuffer.fill(64)(-1)
                 val d = ListBuffer.fill(64)(-1)
@@ -37,6 +33,7 @@ class DeductionTest extends FlatSpec with ChiselScalatestTester with Matchers {
                 for (j <- 0 until 64) {
                     w(j) = if (c.io.write(j).peek().litToBoolean) 1 else 0
                     d(j) = c.io.out(j).peek().litValue.toInt
+                    // Put the output in the correct place
                     if (c.io.write(j).peek().litToBoolean) {
                         out(c.io.pos(j).peek().litValue.toInt) = c.io.out(j).peek().litValue.toInt
                     }
@@ -49,6 +46,16 @@ class DeductionTest extends FlatSpec with ChiselScalatestTester with Matchers {
 
             println(out)
 
+
+            // Load what the data should look like
+            val solution = new ListBuffer[Int]()
+            val src2 = Source.fromFile("solution.txt")
+            for (n <- src2.getLines) {
+                solution.append(n.toInt)
+            }
+            src2.close()
+
+            // Check if it matches
             for (i <- 0 until solution.length) {
                 assert(out(i) == solution(i))
             }
