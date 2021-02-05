0bade62e7878704fcf2c142b08646b9cd31adc73
Sebastian Strempfer <sebastian@strempfer.com>
2021-02-05 12:41:53 -0600

    More exhaustive test

--- src/test/scala/lcomp/PatternEncoderTest.scala
+++ src/test/scala/lcomp/PatternEncoderTest.scala
@@ -8,9 +8,40 @@ import scala.collection.mutable.ListBuffer
 import scala.math.max
 
 class PatternEncoderTest extends FlatSpec with ChiselScalatestTester with Matchers {
+    def testPattern(c: PatternEncoder, pattern: String) {
+        c.io.in.poke(Integer.parseInt(pattern, 2).U)
+        val encoded = c.io.out.peek().litValue().toInt
+        val pos = encoded >> 3
+        val len = encoded & 7
+
+        // Reconstruct input from encoded output
+        var rstring = ""
+        for (i <- 0 until 15) {
+            if (14-i >= pos && 14-i < pos + len) {
+                rstring += "1"
+            } else {
+                rstring += "0"
+            }
+        }
+
+        // Check if reconstructed input matches input
+        val canenc = Integer.parseInt(rstring, 2).toBinaryString == pattern
+        //assert((c.io.canencode.peek().litValue > 0) == canenc)
+
+        // Check if the module agrees
+        c.io.canencode.expect(canenc.B)
+        println("Checked " + pattern + " -> " + (if (canenc) "yes" else "no"))
+    }
+
     it should "test-merge-weird" in {
         // test case body here
         test(new PatternEncoder) { c =>
+            // Simply check every single possible input
+            for (i <- 0 to 32767) {
+                testPattern(c, i.toBinaryString)
+            }
+
+            /*
             c.io.in.poke(Integer.parseInt("111111111111111", 2).U)
             println(c.io.canencode.peek().litValue())
             println(c.io.out.peek().litValue().toInt.toBinaryString)
@@ -49,7 +80,7 @@ class PatternEncoderTest extends FlatSpec with ChiselScalatestTester with Matche
             c.io.in.poke(Integer.parseInt("100000000000000", 2).U)
             println(c.io.canencode.peek().litValue())
             println(c.io.out.peek().litValue().toInt.toBinaryString)
-            c.io.canencode.expect(1.B)
+            c.io.canencode.expect(1.B)*/
         }
     }
 }
